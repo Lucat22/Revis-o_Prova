423316066a008cc191057ad39b271d4b02572ec0
lucat22 <lucalvins1@gmail.com>
2024-04-12 21:50:43 -0300

    Revisão Prova

--- db/conn.js
+++ db/conn.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose')
 async function main(){
 
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PWD}@cluster0.ae8ng2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-        console.log('Banco OK');
-    } catch (error) {
-        console.log('Erro: ' + error);
-    }
+    await mongoose.connect(`mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PWD}@cluster0.jpof0et.mongodb.net/?retryWrites=true&w=majority`)
+    console.log('Banco OK');
+    
+} catch (error) {
+    console.log('Erro: ' + error)
+}
+
 }
 
 module.exports = main
